91810e81f158ce8d7fdcd28067dc354e85b39064
SSimonPhd <simon_phd@protonmail.com>
2023-01-12 07:33:16 -0600

    Updated text

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -10,11 +10,12 @@ const About = () => {
       <p className='profile-text'> <br></br>
         Hi! I'm Scott.</p>
 				<p className='profile-text2'> <br></br>
-        Multidisciplinary Full Stack Web Developer and Data Analyst with a talent for condensing large data into a simple to understand story and building websites from mockup to rollout. 
+        Information Scientist/Web Developer with a hard focus on Blockchain, DeFi, and Web3.  
 				<br></br><br></br>  
-				Adept at working independently as well as collaborating with teams across multiple functions to break down concepts in layman terms. 
+				Relevant experience in Information Architecture and Cloud Services. 
 				<br></br><br></br>  
-				Deliver experience obtained through a cross-functional organization to streamline data analysis, reporting, and auditing. Prepared to excel in solving complex problems.  <br></br> <br></br>
+				Published researcher applying math and logic to solve real world problems.
+				<br></br> <br></br>
         </p>
     </div>
   )
